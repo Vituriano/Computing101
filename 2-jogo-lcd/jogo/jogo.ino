e0ff219880a311c49c2a890d097a61bdb82b6b6b
RicardoMorato <56000167+RicardoMorato@users.noreply.github.com>
2019-10-10 15:37:52 -0300

    Segunda nave adicionada ao jogo.

--- 2-jogo-lcd/jogo/jogo.ino
+++ 2-jogo-lcd/jogo/jogo.ino
@@ -1,7 +1,6 @@
 /*
    ATENÇÂO: Este código é feio e cabeludo, mas ele já sabe disso.
    Um dia vamos arrumar o coitado.
-
 */
 
 #include <LiquidCrystal.h>
@@ -15,6 +14,49 @@
 #define BRIGHT 7
 
 #define min(a,b) ((a<b)?(a):(b))
+
+// NAVES:
+byte fundoInimigo1[8] = {
+  B11000,
+  B10100,
+  B10110,
+  B11111,
+  B10110,
+  B10100,
+  B11000,
+};
+
+byte frenteInimigo1[8] = {
+  B11110,
+  B00001,
+  B00001,
+  B11110,
+  B00001,
+  B00001,
+  B11110,
+};
+
+byte fundoInimigo2[8] = {
+  B01100,
+  B11010,
+  B10011,
+  B11111,
+  B10011,
+  B11010,
+  B01100,
+};
+
+byte frenteInimigo2[8] = {
+  B11100,
+  B00010,
+  B00001,
+  B01111,
+  B00001,
+  B00010,
+  B11100,
+};
+
+// CAMINHOES:
 
 byte fundo[8] = {
   B01100,
