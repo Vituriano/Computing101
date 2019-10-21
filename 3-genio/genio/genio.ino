7d696b5a0cf4a2f4404759f0adf4cd7457bdc197
gbrls <gabrielschneider100@protonmail.com>
2019-10-21 15:05:32 -0300

    Trabalho feito no lh

--- 3-genio/genio/genio.ino
+++ 3-genio/genio/genio.ino
@@ -2,8 +2,11 @@
 #define BTNS 3
 //int jogo[19][3]={0};
 // auto-explicativo
-int buttons[BTNS]={A0, A1, A2},estagio=0,pos=0;
+int buttons[BTNS]={A0, A1, A2},estagio=1,pos=0;
 int leds[BTNS]={8,9,10};
+
+// estado do botão
+int pressionado=0;
 
 void gerarJogo(int** mat){
     for(int i=0;i<RODADAS;i++){
@@ -53,13 +56,14 @@ int lerInput(){
 }
 
 int checarJogada(int** mat, int jogada){
+
     int target=0;
     for(int i=0;i<BTNS;i++){
         if((*(*(mat+pos)+i))) target|= (1<<i);
     }
     Serial.print("Target: ");
     Serial.println(target);
-    return !(jogada^target);
+    return (jogada==target);
 }
 
 void proximaPosicao(int** jogo){
@@ -67,17 +71,22 @@ void proximaPosicao(int** jogo){
         if(++estagio==RODADAS) Serial.println("ACABOU-se");
         printCres(jogo);
         pos=0;
+        
     }
 }
 
 void printCres(int** jogo){
+    //Serial.println("Printing");
+    delay(200);
     for(int i=0;i<estagio;i++){
         for(int j=0;j<BTNS;j++){
             if(*(*(jogo+i)+j)) {
+                //Serial.print("led: ");
+                //Serial.println(j);
                 led(j, 1);
                 delay(1000);
                 led(j, 0);
-                delay(500);
+                if(!(i==estagio-1&&j==BTNS-1))delay(500);
 
             }
         }
@@ -96,6 +105,32 @@ void led(int idx, int state){
     digitalWrite(leds[idx], state?HIGH:LOW);
 }
 
+void reiniciarJogo(int** jogo){
+    for(int i=0;i<BTNS;i++){
+        digitalWrite(leds[i], HIGH);
+    }
+
+    delay(2000);
+    iniciarJogo(jogo);
+}
+
+void iniciarJogo(int** jogo){
+    gerarJogo(jogo);
+    estagio=1;
+    pos=0;
+    printJogo(jogo);
+    desenharJogo(jogo);
+    printCres(jogo);
+}
+
+void printDebug() {
+
+    Serial.print("Estagio: ");
+    Serial.print(estagio);
+    Serial.print(" pos ");
+    Serial.println(pos);
+}
+
 int** jogo=NULL;
 
 void setup() {
@@ -106,9 +141,9 @@ void setup() {
     }
 
     jogo = alocarJogo();
-    gerarJogo(jogo);
-    desenharJogo(jogo);
-    printCres(jogo);
+    iniciarJogo(jogo);
+    //desenharJogo(jogo);
+    //printCres(jogo);
 }
 
 void loop() { 
@@ -116,11 +151,19 @@ void loop() {
     //digitalWrite(9, HIGH);
     int in = lerInput();
     if(in){
-        Serial.println(in);
-        if(checarJogada(jogo, in)){
-            proximaPosicao(jogo);
+        //Serial.println(in);
+        if(!pressionado){
+            Serial.println("Pressionado");
+            if(checarJogada(jogo, in)){
+                printDebug();
+                proximaPosicao(jogo);
+            } else {
+                Serial.println("Errou");
+                //reiniciarJogo(jogo);
+            }
         }
+        pressionado=1;
+    } else {
+        pressionado=0;
     }
-
-    delay(10);
 }
